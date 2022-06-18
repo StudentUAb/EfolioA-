0519e7dad83effb78c5e3eca17ee5a54f0c66184
Ivo Baptista <ivobass@gmail.com>
2022-06-18 22:18:15 +0100

    Add files via upload

---
+++ auxiliares.h
@@ -0,0 +1,11 @@
+#ifndef AUXILIARES_H
+#define AUXILIARES_H
+
+    // Funções 'principais', chamadas a partir do menu
+    void inscrever(void);
+    void anular(void);
+    void gravar(void);
+    void listagem(void);
+    void propinas(void);
+    void sair(void);
+#endif
